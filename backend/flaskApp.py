9023fefef6bda307a976f157c4e7b263ab40dead
ClovertaTheTrilobita <cloverta@petalmail.com>
2025-09-26 15:28:20 +0000

    refactor(api):修改前端接口逻辑

--- backend/flaskApp.py
+++ backend/flaskApp.py
@@ -37,7 +37,7 @@ r = redis.Redis(host="127.0.0.1", port=6379, db=0)
 
 # 启动测试服务器
 def run_dev():
-    app.run(host="10.147.18.141", port=5000)
+    app.run(host="192.168.193.141", port=5000)
 
 def set_status(task_id, status, **extra):
     payload = {"status": status, "updated_at": datetime.datetime.utcnow().isoformat(), **extra}
