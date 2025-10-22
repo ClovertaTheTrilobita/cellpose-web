09d07561843f7c00741791e0f07f6c11395c5b86
ClovertaTheTrilobita <cloverta@petalmail.com>
2025-10-22 14:47:22 +0000

    fix(api): 删除注释

--- frontend/api.js
+++ frontend/api.js
@@ -1,11 +1,9 @@
 const config = {
-  /* 请根据需求修改下列IP和端口 */
   server: {
-    protocol: 'http', // 网络协议
-    host: '192.168.193.141', // 主机IP
-    port: 5000 // 后端运行端口
+    protocol: 'http',
+    host: '192.168.193.141',
+    port: 5000
   }
 };
 
-/* 生成API链接 */
 const API_BASE = `${config.server.protocol}://${config.server.host}:${config.server.port}/`;
